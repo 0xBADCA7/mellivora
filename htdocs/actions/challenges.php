82d826af8b0ebec8c1d769eb691eb2ddaaee829b
MoonfireSeco <haaloplaysminecraft@gmail.com>
2019-04-11 21:51:05 +0300

    Update challenges.php

--- htdocs/actions/challenges.php
+++ htdocs/actions/challenges.php
@@ -165,15 +165,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         //also update the challenge itself
         if ($correct == true) {
             if ($challenge['decay'] != 0 && $challenge['min_points'] != 0) { //is Dynamic Challenge
-                $new_points = (($challenge['min_points'] - $challenge['init_points']) / ($challenge['decay'] * $challenge['decay'])) * ($challenge['solves'] * $challenge['solves']) + $challenge['init_points'];
-                db_update(
-                    'challenges',
-                    array(
-                        'points' => $new_points,
-                        'solves' => $challenge['solves'] + 1
-                    ),
-                    array('id' => $_POST['challenge'])
-                );
+		if ($challenge['solves'] <= $challenge['decay']) {
+			$new_points = (($challenge['min_points'] - $challenge['init_points']) / ($challenge['decay'] * $challenge['decay'])) * ($challenge['solves'] * $challenge['solves']) + $challenge['init_points'];
+			db_update(
+			    'challenges',
+			    array(
+				'points' => $new_points,
+				'solves' => $challenge['solves'] + 1
+			    ),
+			    array('id' => $_POST['challenge'])
+			);
+		}
             }
         }
         if (!$challenge['automark']) {
